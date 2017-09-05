34c0771315a460fd6bf5cb560e951444ff6bea24
Marina <thearnica@gmail.com>
2017-09-05 20:29:02 +1000

    update popup logic

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
+var index = document.querySelector(".index-wrapper");
 var link = document.querySelector(".writeus-link");
-var popup = document.querySelector(".modal-feedback");
-var close = popup.querySelector(".modal-close");
-
-var form = popup.querySelector("form");
+var feedbackpopup = document.querySelector(".modal-feedback");
+var close = feedbackpopup.querySelector(".modal-close");
+var form = feedbackpopup.querySelector("form");
 var nickname = form.querySelector(".field-name");
 var mail = form.querySelector(".field-mail");
 var message = form.querySelector(".field-message");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  feedbackpopup.classList.add("modal-show");
+  index.classList.add("modal-open");
   nickname.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  feedbackpopup.classList.remove("modal-show");
+  index.classList.remove("modal-open");
 });
 
 form.addEventListener("submit", function (evt) {
@@ -44,8 +46,10 @@ form.addEventListener("submit", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
+    var popup = document.querySelector(".modal-show");
+    if (popup) {
       popup.classList.remove("modal-show");
+      index.classList.remove("modal-open");
     }
   }
 });
@@ -57,17 +61,12 @@ var mapclose = mappopup.querySelector(".modal-close");
 maplink.addEventListener("click", function (evt) {
   evt.preventDefault();
   mappopup.classList.add("modal-show");
+  index.classList.add("modal-open");
 });
 
 mapclose.addEventListener("click", function (evt) {
   evt.preventDefault();
   mappopup.classList.remove("modal-show");
+  index.classList.remove("modal-open");
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (mappopup.classList.contains("modal-show")) {
-      mappopup.classList.remove("modal-show");
-    }
-  }
-});
